4258fb6434aedf879aa11e60ebbfae167ccd7c33
Alexandre DROUIN <ale.drouin@gmail.com>
2017-02-18 17:20:51 +0100

    Hypothese 1 - Etape b
    Les nombres autorisés sont les nombres entier de -273 à 5526. Si un nombre
    ne respecte pas cette règle alors il faut lever une exception avec un message précisant le nombre fautif.

--- src/main/java/NumFinder.java
+++ src/main/java/NumFinder.java
@@ -5,11 +5,31 @@ import java.util.ArrayList;
  */
 public class NumFinder {
 
+    /** Minimum authorized value in list. */
+    private final static int MINIMUM_VALUE = -273;
+
+    /** Maximum authorized value in list. */
+    private final static int MAXIMUM_VALUE = 5526;
+
+    private final static String ILLEGAL_ARGUMENT =  "Le nombre %1$s dans la liste n'est pas compris entre %2$s et %3$s";
+
     /**
      * Find the closest number to zero in ArrayList
      * - Empty or null list return 0
+     * - Only number between -273 and 5526 are accepted
      */
-    public int findClosestZero(ArrayList<Integer> values) {
+    public int findClosestZero(ArrayList<Integer> values) throws IllegalArgumentException{
+
+        if (values != null) {
+
+            for (Integer value : values) {
+                if (value < MINIMUM_VALUE || value > MAXIMUM_VALUE) {
+                    throw new IllegalArgumentException(String.format(ILLEGAL_ARGUMENT, value, MINIMUM_VALUE, MAXIMUM_VALUE));
+                }
+            }
+
+        }
+
         return 0;
     }
 
--- src/test/java/NumFinderTest.java
+++ src/test/java/NumFinderTest.java
@@ -22,4 +22,35 @@ public class NumFinderTest {
         Assert.assertEquals(0, numFinder.findClosestZero(null));
     }
 
+
+    @Test
+    public void minimum_value_is_negative_273() {
+
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        values.add(-99999);
+
+        try {
+            numFinder.findClosestZero(values);
+            Assert.fail();
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("Le nombre -99999 dans la liste n'est pas compris entre -273 et 5526", e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void maximum_value_is_5526() {
+
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        values.add(99999);
+
+        try {
+            numFinder.findClosestZero(values);
+            Assert.fail();
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals("Le nombre 99999 dans la liste n'est pas compris entre -273 et 5526", e.getMessage());
+        }
+
+    }
+
 }
